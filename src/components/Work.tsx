3ff7aeb6bff049765b91ae574b6a1a619baa76ba
zohaibyounas <zohaiby737@gmail.com>
2024-11-20 16:53:10 +0500

    updated

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -149,7 +149,7 @@ const Work = () => {
                 React JS Application
               </span>
               <div className="pt-8 text-center">
-                <a href="/">
+                <a href="https://www.iwizsol.com/">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
