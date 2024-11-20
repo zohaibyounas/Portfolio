77bac70c9031e731fa1f4c186c3486fdc99c3f93
zohaibyounas <zohaiby737@gmail.com>
2024-11-20 16:35:06 +0500

    updated

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -11,7 +11,7 @@ const Work = () => {
   return (
     <div className="w-full md:h-screen text-gray-300 ">
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
-        <div className="mt-32 sm:mt-64">
+        <div className="mt-32 sm:mt-72">
           <p className="text-4xl font-bold inline border-b-4 text-black border-pink-600">
             Work
           </p>
