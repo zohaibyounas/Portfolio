efcb16f17a38b44fa69eacdf062027ebdd544a43
zohaibyounas <zohaiby737@gmail.com>
2025-08-08 19:58:33 +0500

    add the email

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -135,7 +135,7 @@ const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]">
             <a
               className="flex justify-between items-center w-full text-gray-200"
-              href="www.linkedin.com/in/zohaib-younas-536755258"
+              href="mailto:zohaiby737@gmail.com"
             >
               Email <HiOutlineMail size={30} />
             </a>
